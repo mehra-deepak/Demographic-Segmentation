6326883e410a78df85256492b658cc8c26957253
Deepak Mehra <mehradeepak0608@gmail.com>
2019-03-11 19:19:19 +0530

    updated source code after data cleaning

--- source code.py
+++ source code.py
@@ -16,5 +16,23 @@ import seaborn as sns
 # importing the dataset
 
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:,1:14].values
+X = dataset.iloc[:,3:13].values
 y = dataset.iloc[:,13:].values
+
+
+# data cleaning
+
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+#creating the dummy variables
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+
+# splitting the dataset in the training set and testing set
